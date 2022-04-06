8dce24b28977ec912eddb7b7a8c4ce91b0d583ed
Zafeer Hafeez <mrh4ck3r1738@gmail.com>
2022-04-06 17:16:20 +0500

    Take city name from CL argument.

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const geocode = require("./utils/geocode"); 
 const weather = require("./utils/weather"); 
 
-geocode("Islamabad", (error, data) => {
-  if (error) {
-    console.log(error);
-  }  else {
-    weather(data.lon, data.lat, (error, data) => {
-      if (error)  {
-        console.log(error);
-      } else {
-        console.log(data);
-      }
-    })
-  }
-});
+const cityName = process.argv[2];
+
+if (cityName) {
+  geocode(cityName, (error, {lon, lat}) => {
+    if (error) {
+      console.log(error);
+    }  else {
+      weather(lon, lat, (error, data) => {
+        if (error)  {
+          console.log(error);
+        } else {
+          console.log(`Status: ${data.weather[0].description}`);
+          console.log(`Temprature: ${data.main.temp}C`);
+          console.log(`Humidity: ${data.main.humidity}%`);
+          console.log(`Visibility: ${+data.visibility/1000}km`);
+        }
+      })
+    }
+  });
+} else {
+  console.log("Please give city name as argument.");
+}
