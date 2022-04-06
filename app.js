a3626f4969b6b1859879570d01e0afb4e2523ec4
Zafeer Hafeez <mrh4ck3r1738@gmail.com>
2022-04-06 05:38:24 +0500

    Integrated both APIs togather one depends on other, even with async execution.

--- app.js
+++ app.js
@@ -1,33 +1,16 @@
-const request = require("request");
+const geocode = require("./utils/geocode"); 
+const weather = require("./utils/weather"); 
 
-const OWAPI = "f939e508817a2bcb3996e293e5f7c251";
-const GCAPI = "pk.eyJ1IjoidmV4MTEyMjMzIiwiYSI6ImNsMW1kcnF3NTBpcnEzZG1tMnJxdzY1NHAifQ.kldgf8YViVjq62vwOg2CGA";
-
-const cityName = "Lahore";
-
-const geoCodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${cityName}.json?access_token=${GCAPI}`;
-request({url:geoCodeURL, json:true}, (error, response) => {
-  // for no internet connection
+geocode("Islamabad", (error, data) => {
   if (error) {
-      console.log("There is an Error in geoCoding Request!");
-  } else if (response.body.features.length === 0) {
-    console.log("No City found with this name.");
-  } else {
-    const coordinates = response.body.features[0].geometry.coordinates;
-    const lon = coordinates[0];
-    const lat = coordinates[1];
-    
-    console.log(`Logitude: ${lon}`)
-    console.log(`Latitude: ${lat}`)
+    console.log(error);
+  }  else {
+    weather(data.lon, data.lat, (error, data) => {
+      if (error)  {
+        console.log(error);
+      } else {
+        console.log(data);
+      }
+    })
   }
 });
-
-// const openWeatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${OWAPI}&units=metric`;
-// request({ url: openWeatherURL, json:true }, (error, weather) => {
-//   try {
-//     const temprature = weather.body.main.temp_max;
-//     console.log(`It is ${+temprature.toFixed(0)} degrees out there.`);
-//   } catch (e) {
-//     console.log(`There is an Error!`);
-//   }
-// });
---
+++ utils/geocode.js
@@ -0,0 +1,22 @@
+const request = require("request");
+
+const geocode = (city, callback) => {
+  const geoCodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${city}.json?access_token=pk.eyJ1IjoidmV4MTEyMjMzIiwiYSI6ImNsMW1kcnF3NTBpcnEzZG1tMnJxdzY1NHAifQ.kldgf8YViVjq62vwOg2CGA`;
+  
+  request({url:geoCodeURL, json:true}, (error, response) => {
+    if (error) { // Connection error
+        callback("There is an Error in geoCoding Request!",undefined);
+    } else if (response.body.features.length === 0) { // If city not found.
+      callback("No City found with this name.", undefined);
+    } else {  // On Success
+      const coordinates = response.body.features[0].geometry.coordinates;
+      const data = {
+        lon: coordinates[0],
+        lat: coordinates[1]
+      };
+      callback(undefined, data);
+    }
+  });
+}
+
+module.exports = geocode
