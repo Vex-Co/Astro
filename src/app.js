d96347667c10c5328c6b59e1e0c2949376d3273a
Zafeer Hafeez <mrh4ck3r1738@gmail.com>
2022-04-08 15:27:30 +0500

    Cleaning the app.js

--- src/app.js
+++ src/app.js
@@ -49,25 +49,3 @@ app.get('/weather', (req, res) => {
 });
 
 app.listen(3000)
-// const cityName = process.argv[2];
-
-// if (cityName) {
-//   geocode(cityName, (error, {lon, lat}) => {
-//     if (error) {
-//       console.log(error);
-//     }  else {
-//       weather(lon, lat, (error, data) => {
-//         if (error)  {
-//           console.log(error);
-//         } else {
-//           console.log(`Status: ${data.weather[0].description}`);
-//           console.log(`Temprature: ${data.main.temp}C`);
-//           console.log(`Humidity: ${data.main.humidity}%`);
-//           console.log(`Visibility: ${+data.visibility/1000}km`);
-//         }
-//       })
-//     }
-//   });
-// } else {
-//   console.log("Please give city name as argument.");
-// }
